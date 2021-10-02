a11312ffbbd762932bb35e2f163c4bda96129a4c
Evgenia Isaeva <evgenia.isaeva297@gmail.com>
2021-10-02 14:07:10 +0300

    TVR#10 Global and Per-Route Guards

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
-import { createApp, reactive } from "vue";
-import App from "./App.vue";
-import router from './router' // <-- This imports index.js from the /router directory
-import store from "./store";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import GStore from './store'
 import 'nprogress/nprogress.css'
 
-const GStore = reactive ({ flashMessage: '' })
-
 createApp(App)
-  .use(store)
   .use(router)
   .provide('GStore', GStore)
   .mount("#app");
