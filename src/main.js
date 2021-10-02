1feb57799b3f4cda9fd098f108288251e80b67e2
Evgenia Isaeva <evgenia.isaeva297@gmail.com>
2021-10-02 12:04:11 +0300

    TVR#9 In-Component Route Guards

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp, reactive } from "vue";
 import App from "./App.vue";
 import router from './router' // <-- This imports index.js from the /router directory
 import store from "./store";
+import 'nprogress/nprogress.css'
 
 const GStore = reactive ({ flashMessage: '' })
 
